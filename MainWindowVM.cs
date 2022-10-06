81784dfdc832583bcd2e93ef81eefcf61c822d93
hybri <hybridwebtech@gmail.com>
2022-10-06 07:26:46 -0600

    First commit

---
+++ MainWindowVM.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RichNoteCore
+{
+    public class MainWindowVM : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler? PropertyChanged;
+
+        private string _documentTitle = "<<new document>>";
+
+        public string DocumentTitle
+        {
+            get => _documentTitle;
+            set
+            {
+                _documentTitle = value;
+                PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(DocumentTitle)));
+            }
+        }
+    }
+}
